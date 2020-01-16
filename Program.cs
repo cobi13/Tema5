ae7bc7477e3c77688536c8bfeda5c35f7417359a
Radu Cobiliac <cobiliac_radu@yahoo.com>
2020-01-16 15:21:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema5
+{
+    class Program
+    {
+        class Mill
+        {
+            private DateTime productionDate;
+            private DateTime expirationDate;
+            public enum EmployeeType
+            {
+                Worker,
+                Driver,
+                Management,
+                Salesman,
+                PR
+            }
+
+            public enum ProductType
+            {
+                Flour,
+                Bran
+            }
+
+            public DateTime ProductionDate
+            {
+                get
+                {
+                    return this.productionDate;
+                }
+
+            }
+            public DateTime ExpirationDate
+            {
+                get
+                {
+                    return this.expirationDate;
+                }
+                set
+                {
+                    this.expirationDate = value;
+                }
+
+            }
+            void SetExpirationDate(ProductType tipProdus)
+            {
+
+            }
+            void Milling()
+            {
+                Console.WriteLine("Milling process");
+            }
+        }
+
+        class Flour : Mill
+        {
+            public enum FlourType
+            {
+                F480,
+                F550,
+                F650,
+                F1350
+            }
+        }
+
+        class Bran : Mill
+        {
+
+        }
+
+        class BagFlour : Flour
+        {
+
+        }
+
+        class BulkFlour : Flour
+        {
+
+        }
+
+        class BagBran : Bran
+        {
+
+        }
+
+        class BulkBran : Bran
+        {
+
+        }
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
